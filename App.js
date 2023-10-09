2e880901df3aaaa0d1fa059203768e1936c31ae1
Kevin <kfaucher@dtcc.edu>
2023-10-08 22:02:41 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Import necessary components from React and React Native.
+import React from 'react';
+import { StyleSheet, View, FlatList, Text, TextInput, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// The main App component as a class.
+export default class App extends React.Component {
+
+  // Initialize the state.
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',         // To store the current text entered into the TextInput.
+      nameList: [],     // To store a list of names.
+    };
+  }
+
+  // Add the current name to the list.
+  addNameToList = () => {
+    const updatedList = this.state.nameList.concat(this.state.name);
+    this.setState({
+      nameList: updatedList,
+      name: '' // Reset the TextInput after adding.
+    });
+  }
+
+  // Render the UI of the component.
+  render() {
+    return (
+        <View style={styles.container}>
+
+          {/* Text input to enter a name */}
+          <TextInput
+              style={styles.input}
+              placeholder="Enter a name"
+              value={this.state.name}
+              onChangeText={(text) => this.setState({ name: text })}
+          />
+
+          {/* Button to add the name */}
+          <Button title="Add Name" onPress={this.addNameToList} />
+
+          {/* List to display the names */}
+          <FlatList
+              data={this.state.nameList}
+              renderItem={({ item }) => <Text style={styles.listItem}>{item}</Text>}
+              keyExtractor={(item, index) => index.toString()}
+          />
+
+        </View>
+    );
+  }
 }
 
+// Styling for the components.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10,
+    marginBottom: 20,
+  },
+  listItem: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
   },
 });
